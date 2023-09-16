b70d314c8551115cfcbd5306ff1bfe0013c7df42
Thales Spanhol <thales_spanhol@hotmail.com>
2023-09-16 19:40:48 -0300

    firebase setup

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,9 +11,9 @@ const firebaseConfig = {
   projectId: "db-inc",
   storageBucket: "db-inc.appspot.com",
   messagingSenderId: "948225132164",
-  appId: "1:948225132164:web:e892d50aa70a6f0d5fe4d1"
+  appId: "1:948225132164:web:e892d50aa70a6f0d5fe4d1",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app)
+export const db = getFirestore(app);
