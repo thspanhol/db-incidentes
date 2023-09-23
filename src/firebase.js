12f9115964926a936782f6c23f1d8bde7da6ddaf
Thales Spanhol <thales_spanhol@hotmail.com>
2023-09-22 21:22:22 -0300

    firebase format

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCgtZLYFAOhwYUBzUZYBzkJk8gMdg8CTkk",
   authDomain: "db-inc.firebaseapp.com",
@@ -14,6 +10,5 @@ const firebaseConfig = {
   appId: "1:948225132164:web:e892d50aa70a6f0d5fe4d1",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const fireDB = getFirestore(app);
